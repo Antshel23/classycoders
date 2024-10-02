949114f77161b02dd62181bd568fc21ced8ac9ff
Antshel23 <antsheldon23@icloud.com>
2024-10-02 12:28:14 +0100

    done so far

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,10 +1,35 @@
 class Employee {
+#salary
+#isHired
 
+constructor(name,title,salary,isHired) {
+this.name = name
+this.title = title
+this.#salary = salary
+this.#isHired = isHired
+}
 
+getSalary() {
+    return this.#salary
+}
 
+setSalary(amount) {
+    this.#salary = amount
+}
+
+getStatus() {
+    return this.#isHired
+}
+
+setStatus(command) {
+    if (command === 'hire') {
+        this.#isHired = true
+    }
+    else if (command === 'fire') {
+        this.#isHired = false
+    }
+}
     
 }
 
-module.exports = {
-    Employee,
-}
+module.exports = Employee
--- classes/Manager.js
+++ classes/Manager.js
@@ -1,10 +1,26 @@
-class Manager {
+const Employee = require('./Employee');
 
+class Manager extends Employee {
+#employeesManaged
 
+constructor(name,title,salary,isHired,department, employeesManaged = []) {
+super(name,title,salary,isHired)
+this.department = department
+this.#employeesManaged = employeesManaged
+}
+
+getEmployeesManaged() {
+    return this.#employeesManaged
+}
+
+setEmployeesManaged(employee) {
+if (employee instanceof Employee) {
+    this.#employeesManaged.push(employee)
+}
+
+}
 
     
 }
 
-module.exports = {
-    Manager,
-}
+module.exports = Manager
