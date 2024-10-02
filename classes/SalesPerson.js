9194e60c7785e419e5be44c850a4a97d4b87e4f5
BenDev181 <benjiro181@gmail.com>
2024-10-02 12:45:00 +0100

    SalesPerson commit

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,19 @@
-class SalesPerson {
+const Employee = require('./Employee');
 
+class SalesPerson extends Employee {
+    #totalSales
 
-
-    
+    constructor(name,title,salary,isHired, clients) {
+        super(name,title,salary,isHired);
+        this.#totalSales = 0;
+        this.clients = clients
+    }
+    getSalesNumbers() {
+        return this.#totalSales;
+    }
+    makeSale(amount) {
+        this.#totalSales = amount
+    }
 }
 
-module.exports = {
-    SalesPerson,
-}
+module.exports = SalesPerson
